053d1d7799cb797189b8d4cc72a445fe8905841f
deekshamypersonal <150110347+deekshamypersonal@users.noreply.github.com>
2024-07-08 11:33:31 -0700

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,25 @@
+import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import './App.css'
+import Header from "./components/Header";
+import AllNews from "./components/AllNews";
+import EveryThingCard from './components/EveryThingCard';
 
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <div className="">
+      <div className="">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<AllNews />} />
+          <Route path="/top-headlines/:category" element={<EveryThingCard />} />
+        </Routes>
+      </BrowserRouter>
+      </div>
+    </div>
+  );
+}
+
+export default App;
